8d80f3573870c00c12ea4b6ae510e6241a5b9107
raushanranjankumar <raushan.ranjan.kumar123@gmail.com>
2024-12-25 16:33:38 +0530

    updated

--- advancejava/src/main/java/org/javaproject/dao/ProductImpl.java
+++ advancejava/src/main/java/org/javaproject/dao/ProductImpl.java
@@ -2,16 +2,22 @@ package org.javaproject.dao;
 
 import org.javaproject.models.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductImpl implements  ProductDAO{
+    List<Product> productList;
+    ProductImpl(){
+        productList = new ArrayList<>();
+    }
     @Override
     public Product addProduct(Product product) {
-        return null;
+        this.productList.add(product);
+        return product;
     }
 
     @Override
     public List<Product> getAllProduct() {
-        return List.of();
+        return productList;
     }
 }
