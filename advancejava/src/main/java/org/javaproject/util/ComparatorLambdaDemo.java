4049cf828bba57ed756c10f7e69eca61c7df784a
raushanranjankumar <raushan.ranjan.kumar123@gmail.com>
2024-12-29 12:52:22 +0530

    updated

--- advancejava/src/main/java/org/javaproject/util/ComparatorLambdaDemo.java
+++ advancejava/src/main/java/org/javaproject/util/ComparatorLambdaDemo.java
@@ -6,7 +6,9 @@ import org.javaproject.dao.ProductImpl;
 import org.javaproject.models.Product;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class ComparatorLambdaDemo {
 
@@ -18,18 +20,35 @@ public class ComparatorLambdaDemo {
         }
 
         // how to Compare without stream
-        Comparator<Product> comparetor = (val1, val2) ->{
-            if(val1.getCost() > val2.getCost())
-                return 1;
-            else if(val1.getCost() < val2.getCost()){
-                return -1;
-            }
-            else return 0;
-        };
-        productDAO.getAllProduct().sort(comparetor.reversed());
-        for(Product product : productDAO.getAllProduct()){
-            System.out.println(product);
-        }
+//        Comparator<Product> comparetor = (val1, val2) ->{
+//            if(val1.getCost() > val2.getCost())
+//                return 1;
+//            else if(val1.getCost() < val2.getCost()){
+//                return -1;
+//            }
+//            else return 0;
+//        };
+//        productDAO.getAllProduct().sort(comparetor.reversed());
+//        for(Product product : productDAO.getAllProduct()){
+//            System.out.println(product);
+//        }
+
+        // Using stream for comparision one way
+//        List<Product> productList = productDAO.getAllProduct().stream()
+//                .sorted(Comparator.comparingDouble(Product::getCost)).collect(Collectors.toList());
+//        productList.forEach(product -> System.out.println(product));
+
+        //// Using stream for comparision another way
+        productDAO.getAllProduct().stream()
+                .sorted((a,b)-> {
+                    if(a.getCost()>b.getCost())
+                        return -1;
+                    if(a.getCost()<b.getCost())
+                        return 1;
+                    else return 0;
+
+                }).forEach(System.out::println);
+
     }
     static Product createProduct(){
         Faker faker = new Faker();
